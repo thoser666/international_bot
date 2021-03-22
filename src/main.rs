8d0ef33a71f54ecc6fd68f358c04a5d4ebc48575
[SC]thoser666[TeEm] <thoser666@web.de>
2021-03-22 17:52:02 +0100

    working_dir wird ausgegeben

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::path::{PathBuf, Display};
+
+fn main()
+{
+    let path = get_working_dir();
+    println!("The working directory is: {:?}" , path);
+}
+
+fn get_working_dir() -> std::io::Result<PathBuf>
+{
+    let path = env::current_dir();
+
+    return path;
+
 }
