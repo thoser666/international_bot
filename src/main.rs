8e75b32d5f6a726636e26914d5872acdf60f900c
[SC]thoser666[TeEm] <thoser666@web.de>
2021-04-10 15:27:02 +0200

    commandline arguments are ok

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,36 @@ fn main()
 {
     let path = get_working_dir();
     println!("The working directory is: {:?}" , path);
+
+    // read command-line parameter
+    let _args: Vec<String> = env::args().collect();
+    let arg = &_args[1];
+
+
+    // Constants
+    let version = "--version";
+    let v = "--v";
+
+
+    if !_args.is_empty()
+    {
+        match arg.to_string()
+        {
+            version =>
+                {
+                    // TODO write Phantombotversion and exit Program
+                    println!("--Version");
+                }
+            v =>
+                {
+                    // TODO write Phantombotversion and exit Program
+                    println!("--v");
+                }
+        }
+
+    }
+    println!("{:?}", _args);
+
 }
 
 fn get_working_dir() -> std::io::Result<PathBuf>
