2ddfd7666c20c5d00b0509ff5a2b72ab3f052cc5
[SC]thoser666[TeEm] <thoser666@web.de>
2021-05-08 13:47:41 +0200

    getter/Setter 4 botversion

--- src/repoversion.rs
+++ src/repoversion.rs
@@ -1,7 +1,30 @@
 pub struct RepoVersion
 {
     international_bot_version : String,
-repoversion : String,
-buildtype : String,
-panel_version : String
+    repo_version : String,
+    build_type : String,
+    panel_version : String
+}
+
+impl RepoVersion
+{
+    pub fn new() -> Self
+    {
+        RepoVersion{
+            international_bot_version: "".to_string(),
+            repo_version: "".to_string(),
+            build_type: "".to_string(),
+            panel_version: "".to_string()
+        }
+    }
+
+    pub fn get_international_bot_version(self) -> String {
+        return self.international_bot_version.clone();
+    }
+
+    pub fn set_international_bot_version(&mut self,international_bot_version: String) {
+        self.international_bot_version = international_bot_version;
+    }
+
+
 }
